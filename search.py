c9a8e0595a992431e456f9488346595c20c1285a
GiorgioKalini <prygovor1@gmail.com>
2022-07-09 18:38:47 +0300

    day 2

--- search.py
+++ search.py
@@ -1,7 +1,9 @@
 """Поиск любых файлов на компьютере."""
+from itertools import count
 import os, shutil
 
 KEY_FOR_SEARCH = input('Что ищем???\n')
+PATH_FOR_COPY = input('Куда копировать файлы?\n')
 
 def search():
     for adress, dirs, files in os.walk(input('Введите путь старта\n')):
@@ -13,7 +15,24 @@ def read_from_pathtxt(path):
     with open(path) as r:
         for i in r:
             if KEY_FOR_SEARCH in i:
-                return path                
+                return copy(path)
+
+def copy(path):
+    file_name = path.split('\\')[-1]
+    count = 1
+    while True:
+        if os.path.isfile(os.path.join(PATH_FOR_COPY, file_name)):
+            file_name = f'({count}).'.join(file_name.split('.'))
+            count += 1
+        else:
+            break
+
+    shutil.copyfile(path, os.path.join(PATH_FOR_COPY, file_name))
+    print('Файл скопирован', file_name)
 
 for i in search():
-    read_from_pathtxt()
+    try:
+        read_from_pathtxt(i)
+    except Exception as e:
+        with open(os.path.join(PATH_FOR_COPY, 'errors.txt'), 'a') as r:
+            r.write(str(e) + '\n' + i + '\n')
